ed5da4c5d6d7ee8326a2495403d316009034c95f
JustTryingToBeBetter <4260354@myuwc.ac.za>
2025-11-04 12:20:14 +0200

    yay yay

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello again from CI/CD pipeline! Yay"
+    return "Hello again from CI/CD pipeline! Yay Yay"
 
 
 if __name__ == '__main__':
