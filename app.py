7d6faab12ba6061adfab8bc7e2267e8b1fb77797
JustTryingToBeBetter <4260354@myuwc.ac.za>
2025-11-04 12:35:21 +0200

    trying to fix bug again

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Again 1"
+    return "Hello Again 2"
 
 
 if __name__ == '__main__':
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello Again 1" in response.data
+    assert b"Hello Again 2" in response.data
